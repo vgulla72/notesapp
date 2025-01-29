17b9d82c34971e3eaa0fcf2ff12fe24375b80e7d
Vasantha Gullapalli <vasanthagullapalli@Vasanthas-MacBook-Air.local>
2025-01-28 23:10:28 -0800

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 2)}>
-         mickey count is {count}
+         amplify mickey count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
