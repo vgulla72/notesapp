2e08afeed5898c273d21dccbf9ad5e764ed51176
vgulla72 <vasantha.gullapalli@gmail.com>
2025-01-29 21:25:42 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={reactLogo} className="logo react" alt="React logo" />
 
-        <h1>Hello from Amplify</h1>
+        <h1>Hello from Mickey Amplify</h1>
       </header>
     </div>
   );
